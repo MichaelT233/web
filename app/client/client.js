86b426c2b075c216bcdeece053ba619e7c956fe4
MichaelT233 <michaelthiele673@gmail.com>
2021-06-01 17:50:40 -0700

    react app barebones

---
+++ app/client/client.js
@@ -0,0 +1,6 @@
+<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js">
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+); 
+</script>
