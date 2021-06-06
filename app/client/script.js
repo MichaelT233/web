070c8ab6da18a6e48420662f1c1130f116cca274
MichaelT233 <michaelthiele673@gmail.com>
2021-06-06 15:09:34 -0700

    forked a prod branch for server testing, changed network data to suit

--- app/client/script.js
+++ app/client/script.js
@@ -9,12 +9,12 @@ ReactDOM.render(ipUI, document.getElementById('root'))
 the ip address is then rendered in the DOM in the div 'root' on h1 */
 function getIP() {
     var request = new XMLHttpRequest()
-    request.open("GET", '//127.0.0.1:3000/findip')
+    request.open("GET", '//127.0.0.1:3000	/findip')
     request.onreadystatechange = function ready() {
         if(request.readyState == XMLHttpRequest.DONE) {
             var publicIP = request.responseText
             console.log(publicIP)
-            ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, publicIP), document.getElementById('root'))
+            ReactDOM.render( /*#__PURE__*/React.createElement("p", null, publicIP), document.getElementById('root'))
         }
     }
     request.send()
