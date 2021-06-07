7c920d48758b8071e4a2da0ff8f04c4038203942
MichaelT233 <michaelthiele673@gmail.com>
2021-06-07 02:27:28 +0000

    finally fixed REST API URL syntax error

--- app/client/script.js
+++ app/client/script.js
@@ -9,7 +9,7 @@ ReactDOM.render(ipUI, document.getElementById('root'))
 the ip address is then rendered in the DOM in the div 'root' on h1 */
 function getIP() {
     var request = new XMLHttpRequest()
-    request.open("GET", '192.168.1.16:3000/findip')
+    request.open("GET", 'findip')
     request.onreadystatechange = function ready() {
         if(request.readyState == XMLHttpRequest.DONE) {
             var publicIP = request.responseText
