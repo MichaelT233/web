ae6405944a1036c1e9afe45b2066727bdf064a04
MichaelT233 <michaelthiele673@gmail.com>
2021-08-11 17:44:10 -0700

    created basic react component to render data fetched from psql database

--- client/script.js
+++ client/script.js
@@ -8,10 +8,19 @@ function get(url, callback) {
     request.onreadystatechange = function ready() {
         if(request.readyState == XMLHttpRequest.DONE) {
             var responseText = request.responseText
-            console.log(responseText)
             const obj = JSON.parse(responseText)
-            console.log(obj.products[0].name)
-            console.log(obj.products[1].price)
+            //initital draft of a react component that renders a single element in what will be a grid view store
+            function Store(props) {
+                return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", null, props.title), /*#__PURE__*/React.createElement("div", null, props.description), /*#__PURE__*/React.createElement("div", null, props.price));
+            } 
+            //calling react component to create view with valued props
+            view = /*#__PURE__*/React.createElement(Store, {
+                title: obj.products[0].name,
+                description: obj.products[0].description,
+                price: obj.products[0].price
+            }); 
+            //rendering finished view into store view
+            ReactDOM.render(view, document.getElementById('store_view'));
         }
     }
     request.send()
