46bdf92ae91949dd0ea02f8fb7897f6d7e8cbb72
MichaelT233 <michaelthiele673@gmail.com>
2021-08-28 09:10:00 -0700

    commenting/documenting

--- client/script.js
+++ client/script.js
@@ -1,37 +1,42 @@
 //needs general error handling
-//needs anti-spam mechanism
 
-// function for making AJAX calls to a given url with a given callback to execute
+// function for making AJAX requests for JSON objects from a given URL and then executes a given callback with access to said JSON data
 function getJSON(url, callback) {
+    // creating AJAX request object
     var request = new XMLHttpRequest()
+    // calling open method, creating connection
     request.open("GET", url)
+    // conditional requiring the request to be completed in order to execute given callback
     request.onreadystatechange = function ready() {
         if(request.readyState == XMLHttpRequest.DONE) {
-            // ASYNC CODE
-            // access JSON object here
-            var responseText = request.responseText
-            const obj = JSON.parse(responseText)
+            // ASYNC
+            // converting the response text (which is in JSON format) into an actual JSON reference
+            const obj = JSON.parse(request.responseText)
+            // callback is.. called, with the JSON as it's argument
             callback(obj)
         }
     }
+    // request being sent to server, executes before async callback above
     request.send()
 }
 
 // function made to load product data and render into elements in the store viewport 
 function loadProducts() {
-    // wrapping this code as a callback to use later in this function
-    function callback_holder(obj) {
+    // wrapping this code as a callback to use later in this function, takes JSON response from product database as it's argument
+    function build_store(obj) {
         // loop that generates the necessary html divs based on product quantity, for react to plug into
         // initial string
-        container = `<div class='item_wrapper' id='store_item0'></div>`;            
+        container = `<div class='item_wrapper' id='store_item0'></div>`;
+        // creating a counter value            
         var i = 1
-        while (i < 10) {
+        // loops as many times as their are products in the JSON response
+        while (i < obj.products.length) {
             // building string (and divs)
             container = container + `<div class='item_wrapper' id='store_item${i}'></div>`
             //increment counter value
             i ++
         }
-        // converting the jsx string into react jsx
+        // converting the jsx string into an actual react jsx reference
         container_jsx = /*#__PURE__*/React.createElement("div", {
             id: "store_wrapper",
             dangerouslySetInnerHTML: {
@@ -55,19 +60,19 @@ function loadProducts() {
         // loop for populating the previously rendered divs with store items/products
         i = 0
         while (i < obj.products.length) { 
-            //calling react component to create store items with product data from the database
+            // calling react component to create store items with product data from the database
             view = /*#__PURE__*/React.createElement(Store, {
                 title: obj.products[i].name,
                 description: obj.products[i].description,
                 price: obj.products[i].price,
                 image_path: obj.products[i].image_path
             }); 
-            //rendering finished item into it's div
+            // rendering finished item into it's div
             ReactDOM.render(view, document.getElementById('store_item' + i));
-            //increment counter value
+            // increment counter value
             i ++
         }
     }
-    // call AJAX json get request using previous component as a callback
-    getJSON('db', callback_holder)
+    // call AJAX JSON get request to product database URL and giving the above function as callback
+    getJSON('db', build_store)
 }
