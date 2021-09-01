a0b1f760e0048d20de55da1b7d53337a817d9388
MichaelT233 <michaelthiele673@gmail.com>
2021-09-01 13:05:41 -0700

    updated testing script

--- dev/Client_Build.sh
+++ dev/Client_Build.sh
@@ -3,9 +3,13 @@
 # transpile react_dev.jsx into plain javascript and save in /client/react.js
 npx babel ./react_dev.jsx --out-file ./react.js
 echo 'babel transpile complete'
+# bundle the multiple javascript files into one
+npx webpack
+# remove intermediary transpiled javascript file
+rm react.js
 # transpile style_dev.scss into plain css and save in /client/style.css (omitting source map file creation)
 #npx sass --no-source-map style_dev.scss ../client/style.css
-npx webpack
+# start node web server for testing and change back to same directory as it started in
 cd ..
 node main.js
 cd dev
