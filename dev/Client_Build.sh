614e4cadd38ed552c63ac00a6bf1b45df76f5ed8
MichaelT233 <michaelthiele673@gmail.com>
2021-09-01 08:52:16 -0700

    created basic testing setup for scss

--- dev/Client_Build.sh
+++ dev/Client_Build.sh
@@ -1,3 +1,6 @@
-# script to automate transpiling of jsx
-# transpile react jsx into plain javascript and save in /client/react.js
-npx babel react_dev.jsx --out-file ../client/react.js
+# script to automate transpiling of jsx and scss
+# uncomment lines as needed
+# transpile react_dev.jsx into plain javascript and save in /client/react.js
+#npx babel react_dev.jsx --out-file ../client/react.js
+# transpile style_dev.scss into plain css and save in /client/style.css (omitting source map file creation)
+npx sass --no-source-map style_dev.scss ../client/style.css
