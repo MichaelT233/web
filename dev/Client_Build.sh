1ce74fe839de03d9f27d733720afd0921317bba0
MichaelT233 <michaelthiele673@gmail.com>
2021-09-19 15:02:24 -0700

    changed css units to em and %

--- dev/Client_Build.sh
+++ dev/Client_Build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # script for transpiling and bundling client resources
 
+# transpile jsx and bundle javascript files
 function build_js {
     # transpile react_dev.jsx into plain javascript as react.js, also include vanilla js files in order to remove comments
     npx babel ./react_dev.jsx --out-file ./react.js
@@ -17,19 +18,20 @@ function build_js {
 }
 # transpile style_dev.scss into plain css and save in /client/style.css (omitting source map file creation and comments (compressed))
 function build_css {
+    # compress output and save to the src folder as style.css
     npx sass --style=compressed --no-source-map style_dev.scss ../src/style.css
     echo 'sass transpile complete'
 }
 
-read -p "enter 1 to build js, enter 2 to build css, enter 3 to do both: " response
+# command line arguments: -js, -css, -all
 # build js
-if [ "$response" == "1" ]; then
+if [ $1 == "-js" ]; then
     build_js
 # build css
-elif [ "$response" == "2" ]; then
+elif [ $1 == "-css" ]; then
     build_css
 # build js and css
-elif [ "$response" == "3" ]; then
+elif [ $1 == "-all" ]; then
     build_js
     build_css
 # invalid respose
