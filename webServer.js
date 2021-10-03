f3e5fc9799e6238e8d27d478656c93408143c4ac
MichaelT233 <michaelthiele673@gmail.com>
2021-10-03 13:20:41 -0700

    refactor

--- webServer.js
+++ webServer.js
@@ -1,27 +1,20 @@
 // node web server
-// load the express module
+
+// load modules
 const express = require('express')
-// create express application object
 const app = express()
+const fs = require('fs')
+const { Pool } = require('pg')
+
 // port number that the http web server will listen on
 const webPort = 80
-// load file system (acesss) module
-const fs = require('fs')
-// load postgreSQL driver module and create constructor for the pg Pool class
-const { Pool } = require('pg')
 // password that will be used for queries to the database
 const dbPassword = 'devPass'
 // port number that the database will listen on
 const dbPort = 5432
-// use src directory as working directory for client resources
-app.use(express.static('src'))
-// application object listen on port value
-app.listen(webPort, () => {
-  	console.log(`web application listening on port ${webPort}...\nCtrl+C to exit`)
-})
+
 // create new instance of Pool object for DB access
 const psql = new Pool({
-    // building object properties
     // local docker env user defined in /infra/siteDeploy.sh
     user: 'postgres',
     // default first IP assigned by docker to containers
@@ -32,65 +25,59 @@ const psql = new Pool({
     port: dbPort,
 })
 
-/***********************************/
+// use src directory as working directory for client resources
+app.use(express.static('src'))
+// application object listen on port value
+app.listen(webPort, () => {
+  	console.log(`web application listening on port ${webPort}...\nCtrl+C to exit`)
+})
 
-// callback to be executed when the / directory is requested
+// GET root
 app.get('/', (req, res) => {
-    // read html file to be served
+    // read and serve html file
     fs.readFile('html/store.html', 'utf8' , (err, data) => {
         if (err) {
             console.error(err)
             return
         }
-        // set response type to HTML
         res.type('html')
-        // serve HTML file
         res.send(data)
     })
 })
 
-/***********************************/
-
-// callback to be executed when the /db directory is requested
-app.get('/all', (req, res) => {
-    // callback for sending reponse
-    function sendResponse(data) {
-        // set response type to text/html
-        res.type('html')
-        // send response
-        res.send(data)
-    }
-    // using Pool instance to query postgreSQL database
-    psql.query('SELECT * FROM products;', (err, res) => {
-        // converting JSON object into a string in order to edit and transfer
-        const rows = JSON.stringify(res.rows)
-        // send response
-        sendResponse(rows)
-    })
-})
-
-// callback to be executed when the /cart directory is requested
+// GET cart
 app.get('/cart', (req, res) => {
-    // read html file to be served
+    // read and serve html file
     fs.readFile('html/cart.html', 'utf8' , (err, data) => {
         if (err) {
             console.error(err)
             return
         }
-        // set response type to HTML
         res.type('html')
-        // serve HTML file
         res.send(data)
     })
-    //console.log(req.cookies)
 })
 
+// GET all
+app.get('/all', (req, res) => {
+    // callback for sending reponse
+    function sendResponse(data) {
+        res.type('html')
+        res.send(data)
+    }
+    // using Pool instance to query postgreSQL database
+    psql.query('SELECT * FROM products;', (err, res) => {
+        // converting JSON object into a string in order to send as response text
+        const rows = JSON.stringify(res.rows)
+        sendResponse(rows)
+    })
+})
+
+// GET cart-data
 app.get('/cart-data', (req, res) => {
     // callback for sending reponse
     function sendResponse(data) {
-        // set response type to text/html
         res.type('html')
-        // send response
         res.send(data)
     }
     // set initial sql string to find product data from database, according to what is in the client's cart
@@ -114,9 +101,8 @@ app.get('/cart-data', (req, res) => {
             console.error(err)
             return
         }
-        // converting JSON object into a string in order to edit and transfer
+        // converting JSON object into a string in order to send as response text
         const rows = JSON.stringify(res.rows)
-        // send response
         sendResponse(rows)
     })
 })
