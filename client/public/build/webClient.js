d41159066b9e533b030d063390c0814f0a1feb46
MichaelT233 <michaelthiele673@gmail.com>
2021-11-09 18:36:45 -0800

    design master controller with read/write layers

--- client/public/build/webClient.js
+++ client/public/build/webClient.js
@@ -519,47 +519,197 @@ var cart = new _store_jsx__WEBPACK_IMPORTED_MODULE_0__.Cart();
 var store = new _store_jsx__WEBPACK_IMPORTED_MODULE_0__.Store();
 var checkout = new _checkout_jsx__WEBPACK_IMPORTED_MODULE_1__.Checkout();
 
-class StateController {
-  setPathState() {
-    switch (location.pathname) {
-      case '/':
+class Controller {
+  constructor() {
+    if (localStorage.getItem('state') == null) {
+      localStorage.setItem('state', 'start');
+    }
+  }
+
+  state = {
+    dom: localStorage.getItem('state'),
+    ui: history.state.name
+  };
+  instructionTable = {
+    dom: [{
+      input: '/',
+      state: 'start',
+      instruction: () => this.writeDOM('store'),
+      newState: 'store'
+    }, {
+      input: '/',
+      state: 'store',
+      instruction: () => this.writeDOM('store'),
+      newState: 'store'
+    }, {
+      input: '/',
+      state: 'checkout',
+      instruction: () => this.writeDOM('store'),
+      newState: 'store'
+    }, {
+      input: '/checkout',
+      state: 'start',
+      instruction: () => location.pathname = '/',
+      newState: 'store'
+    }, {
+      input: '/checkout',
+      state: 'store',
+      instruction: () => this.writeDOM('checkout'),
+      newState: 'checkout'
+    }, {
+      input: '/checkout',
+      state: 'checkout',
+      instruction: () => this.writeDOM('checkout'),
+      newState: 'checkout'
+    }],
+    ui: [{
+      input: 'clickTitle',
+      state: 'home',
+      instruction: () => this.writeUI('home'),
+      newState: 'home'
+    }, {
+      input: 'clickTitle',
+      state: 'cart',
+      instruction: () => this.writeUI('home'),
+      newState: 'home'
+    }, {
+      input: 'clickTitle',
+      state: 'search',
+      instruction: () => this.writeUI('home'),
+      newState: 'home'
+    }, {
+      input: 'clickTitle',
+      state: 'category',
+      instruction: () => this.writeUI('home'),
+      newState: 'home'
+    }, {
+      input: 'clickCart',
+      state: 'home',
+      instruction: () => this.writeUI('cart'),
+      newState: 'cart'
+    }, {
+      input: 'clickCart',
+      state: 'cart',
+      instruction: () => this.writeUI('cart'),
+      newState: 'cart'
+    }, {
+      input: 'clickCart',
+      state: 'search',
+      instruction: () => this.writeUI('cart'),
+      newState: 'cart'
+    }, {
+      input: 'clickCart',
+      state: 'category',
+      instruction: () => this.writeUI('cart'),
+      newState: 'cart'
+    }, {
+      input: 'clickCategory',
+      state: 'home',
+      instruction: params => this.writeUI('category', params),
+      newState: 'category'
+    }, {
+      input: 'clickCategory',
+      state: 'cart',
+      instruction: params => this.writeUI('category', params),
+      newState: 'category'
+    }, {
+      input: 'clickCategory',
+      state: 'search',
+      instruction: params => this.writeUI('category', params),
+      newState: 'category'
+    }, {
+      input: 'clickCategory',
+      state: 'category',
+      instruction: params => this.writeUI('category', params),
+      newState: 'category'
+    }, {
+      input: 'search',
+      state: 'home',
+      instruction: params => this.writeUI('search', params),
+      newState: 'search'
+    }, {
+      input: 'search',
+      state: 'cart',
+      instruction: params => this.writeUI('search', params),
+      newState: 'search'
+    }, {
+      input: 'search',
+      state: 'search',
+      instruction: params => this.writeUI('search', params),
+      newState: 'search'
+    }, {
+      input: 'search',
+      state: 'category',
+      instruction: params => this.writeUI('search', params),
+      newState: 'search'
+    }, {
+      input: 'popstate',
+      state: 'home',
+      instruction: () => store.loadAll(),
+      newState: 'home'
+    }, {
+      input: 'popstate',
+      state: 'cart',
+      instruction: () => cart.load(),
+      newState: 'cart'
+    }, {
+      input: 'popstate',
+      state: 'search',
+      instruction: () => {
+        store.loadSearch(history.state.pattern);
+        document.getElementById('searchBar').value = history.state.pattern;
+      },
+      newState: 'search'
+    }, {
+      input: 'popstate',
+      state: 'category',
+      instruction: () => store.loadCategory(history.state.category),
+      newState: 'category'
+    }]
+  };
+
+  controlLayer(layer, input, params) {
+    if (layer == 'ui') {
+      this.state.ui = history.state.name;
+    }
+
+    for (const row of this.instructionTable[layer]) {
+      if (input == row.input && this.state[layer] == row.state) {
+        console.log(row.instruction);
+        row.instruction(params);
+
+        if (layer == 'dom') {
+          localStorage.setItem('state', row.newState);
+        }
+
+        this.state[layer] = row.newState;
+        return;
+      }
+    }
+  }
+
+  writeDOM(state) {
+    switch (state) {
+      case 'store':
         window.addEventListener('load', () => {
           document.getElementById('mainTitle').addEventListener('click', () => {
-            store.loadAll();
-            history.pushState({
-              name: 'home'
-            }, 'Home');
-            console.log(history.state.name);
+            this.controlLayer('ui', 'clickTitle');
           });
           document.getElementById('cartIcon').addEventListener('click', () => {
-            cart.load();
-            history.pushState({
-              name: 'cart'
-            }, 'Cart');
-            console.log(history.state.name);
+            this.controlLayer('ui', 'clickCart');
           });
           var menu = document.getElementById('dropdownContent');
 
           for (let i = 0; i < menu.children.length; i++) {
             menu.children[i].addEventListener('click', () => {
-              store.loadCategory(`category${i}`);
-              history.pushState({
-                name: 'category',
-                category: `category${i}`
-              }, `Category${i}`);
-              console.log(history.state.name);
+              this.controlLayer('ui', 'clickCategory', i);
             });
           }
 
           var searchBar = document.getElementById('searchBar');
           searchBar.addEventListener('change', () => {
             const pattern = searchBar.value;
-            store.loadSearch(pattern);
-            history.pushState({
-              name: 'search',
-              pattern: pattern
-            }, 'Search');
-            console.log(history.state.name);
+            this.controlLayer('ui', 'search', pattern);
           });
           document.getElementById('menuIcon').addEventListener('click', () => {
             store.displayDropdown();
@@ -568,20 +718,18 @@ class StateController {
           dropdownContent.addEventListener('click', () => {
             dropdownContent.className = 'dropdownContentOff';
           });
-          this.setStoreState();
-          console.log(history.state.name);
+          this.controlLayer('ui', 'popstate');
         });
         window.addEventListener('popstate', () => {
           if (history.state != null) {
-            this.setStoreState();
-            console.log(history.state.name);
+            this.controlLayer('ui', 'popstate');
           } else {
             history.go(-1);
           }
         });
         break;
 
-      case '/checkout':
+      case 'checkout':
         window.addEventListener('load', () => {
           checkout.load();
           document.getElementById('backIcon').addEventListener('click', () => {
@@ -592,31 +740,49 @@ class StateController {
     }
   }
 
-  setStoreState() {
-    switch (history.state.name) {
-      case 'home':
-        store.loadAll();
-        break;
+  writeUI(pageState, params) {
+    switch (this.state.dom) {
+      case 'store':
+        switch (pageState) {
+          case 'home':
+            store.loadAll();
+            history.pushState({
+              name: 'home'
+            }, 'Home');
+            break;
 
-      case 'category':
-        store.loadCategory(history.state.category);
-        break;
+          case 'category':
+            store.loadCategory(`category${params}`);
+            history.pushState({
+              name: 'category',
+              category: `category${params}`
+            }, `Category${params}`);
+            break;
 
-      case 'cart':
-        cart.load();
-        break;
+          case 'cart':
+            cart.load();
+            history.pushState({
+              name: 'cart'
+            }, 'Cart');
+            break;
 
-      case 'search':
-        store.loadSearch(history.state.pattern);
-        document.getElementById('searchBar').value = history.state.pattern;
+          case 'search':
+            store.loadSearch(params);
+            history.pushState({
+              name: 'search',
+              pattern: params
+            }, 'Search');
+            break;
+        }
+
         break;
     }
   }
 
 }
 
-var control = new StateController();
-control.setPathState();
+var controller = new Controller();
+controller.controlLayer('dom', location.pathname);
 
 })();
 
--- client/src/webClient.js
+++ client/src/webClient.js
@@ -4,38 +4,86 @@ import {Checkout} from './checkout.jsx'
 var cart = new Cart()
 var store = new Store()
 var checkout = new Checkout()
-// main control mechanism
-class StateController {
-    // browser url path is state of this unit
-    setPathState() {
-        switch (location.pathname) {
-            case '/':
+
+class Controller {
+    constructor() {
+        if (localStorage.getItem('state') == null) {
+            localStorage.setItem('state', 'start')
+        }
+    }
+    state = {
+        dom: localStorage.getItem('state'),
+        ui: history.state.name
+    }
+    instructionTable = {
+        dom: [
+            {input: '/', state: 'start', instruction: ()=>this.writeDOM('store'), newState: 'store'},
+            {input: '/', state: 'store', instruction: ()=>this.writeDOM('store'), newState: 'store'},
+            {input: '/', state: 'checkout', instruction: ()=>this.writeDOM('store'), newState: 'store'},
+            {input: '/checkout', state: 'start', instruction: ()=>location.pathname='/', newState: 'store'},
+            {input: '/checkout', state: 'store', instruction: ()=>this.writeDOM('checkout'), newState: 'checkout'},
+            {input: '/checkout', state: 'checkout', instruction: ()=>this.writeDOM('checkout'), newState: 'checkout'},
+        ],
+        ui: [
+            {input: 'clickTitle', state: 'home', instruction: ()=>this.writeUI('home'), newState: 'home'},
+            {input: 'clickTitle', state: 'cart', instruction: ()=>this.writeUI('home'), newState: 'home'},
+            {input: 'clickTitle', state: 'search', instruction: ()=>this.writeUI('home'), newState: 'home'},
+            {input: 'clickTitle', state: 'category', instruction: ()=>this.writeUI('home'), newState: 'home'},
+            {input: 'clickCart', state: 'home', instruction: ()=>this.writeUI('cart'), newState: 'cart'},
+            {input: 'clickCart', state: 'cart', instruction: ()=>this.writeUI('cart'), newState: 'cart'},
+            {input: 'clickCart', state: 'search', instruction: ()=>this.writeUI('cart'), newState: 'cart'},
+            {input: 'clickCart', state: 'category', instruction: ()=>this.writeUI('cart'), newState: 'cart'},
+            {input: 'clickCategory', state: 'home', instruction: (params)=>this.writeUI('category', params), newState: 'category'},
+            {input: 'clickCategory', state: 'cart', instruction: (params)=>this.writeUI('category', params), newState: 'category'},
+            {input: 'clickCategory', state: 'search', instruction: (params)=>this.writeUI('category', params), newState: 'category'},
+            {input: 'clickCategory', state: 'category', instruction: (params)=>this.writeUI('category', params), newState: 'category'},
+            {input: 'search', state: 'home', instruction: (params)=>this.writeUI('search', params), newState: 'search'},
+            {input: 'search', state: 'cart', instruction: (params)=>this.writeUI('search', params), newState: 'search'},
+            {input: 'search', state: 'search', instruction: (params)=>this.writeUI('search', params), newState: 'search'},
+            {input: 'search', state: 'category', instruction: (params)=>this.writeUI('search', params), newState: 'search'},
+            {input: 'popstate', state: 'home', instruction: ()=>store.loadAll(), newState: 'home'},
+            {input: 'popstate', state: 'cart', instruction: ()=>cart.load(), newState: 'cart'},
+            {input: 'popstate', state: 'search', instruction: ()=>{store.loadSearch(history.state.pattern);document.getElementById('searchBar').value=history.state.pattern}, newState: 'search'},
+            {input: 'popstate', state: 'category', instruction: ()=>store.loadCategory(history.state.category), newState: 'category'}
+        ]
+    }
+    controlLayer(layer, input, params) {
+        if (layer == 'ui') {
+            this.state.ui = history.state.name
+        }
+        for (const row of this.instructionTable[layer]) {
+            if (input == row.input && this.state[layer] == row.state) {
+                console.log(row.instruction)
+                row.instruction(params)
+                if (layer == 'dom') {
+                    localStorage.setItem('state', row.newState)
+                }
+                this.state[layer] = row.newState
+                return
+            }
+        }
+    }
+    writeDOM(state) {
+        switch (state) {
+            case 'store':
                 window.addEventListener('load', () => {
                     /***********************************/
                     document.getElementById('mainTitle').addEventListener('click', () => {
-                        store.loadAll()
-                        history.pushState({name: 'home'}, 'Home')
-                        console.log(history.state.name)
+                        this.controlLayer('ui', 'clickTitle')
                     })
                     document.getElementById('cartIcon').addEventListener('click', () => {
-                        cart.load()
-                        history.pushState({name: 'cart'}, 'Cart')
-                        console.log(history.state.name)
+                        this.controlLayer('ui', 'clickCart')
                     })
                     var menu = document.getElementById('dropdownContent')
                     for (let i = 0; i < menu.children.length; i++) {
                         menu.children[i].addEventListener('click', () => {
-                            store.loadCategory(`category${i}`)
-                            history.pushState({name: 'category', category: `category${i}`}, `Category${i}`)
-                            console.log(history.state.name)
+                            this.controlLayer('ui', 'clickCategory', i)
                         })
                     }
                     var searchBar = document.getElementById('searchBar')
                     searchBar.addEventListener('change', () => {
                         const pattern = searchBar.value
-                        store.loadSearch(pattern)
-                        history.pushState({name: 'search', pattern: pattern}, 'Search')
-                        console.log(history.state.name)
+                        this.controlLayer('ui', 'search', pattern)
                     })
                     document.getElementById('menuIcon').addEventListener('click', () => {
                         store.displayDropdown()
@@ -45,20 +93,18 @@ class StateController {
                         dropdownContent.className = 'dropdownContentOff'
                     })
                     /***********************************/
-                    this.setStoreState()
-                    console.log(history.state.name)
+                    this.controlLayer('ui', 'popstate')
                 })
                 window.addEventListener('popstate', () => {
                     if (history.state != null) {
-                        this.setStoreState()
-                        console.log(history.state.name) 
+                        this.controlLayer('ui', 'popstate')
                     }
                     else {
                         history.go(-1)
                     }
                 })
                 break
-            case '/checkout':
+            case 'checkout':
                 window.addEventListener('load', () => {
                     checkout.load()
                     document.getElementById('backIcon').addEventListener('click', () => {
@@ -68,24 +114,30 @@ class StateController {
                 break
         }
     }
-    setStoreState() {
-        switch (history.state.name) {
-            case 'home':
-                store.loadAll()
-                break
-            case 'category':
-                store.loadCategory(history.state.category)
-                break
-            case 'cart':
-                cart.load()
-                break
-            case 'search':
-                store.loadSearch(history.state.pattern)
-                document.getElementById('searchBar').value = history.state.pattern
+    writeUI(pageState, params) {
+        switch (this.state.dom) {
+            case 'store':
+                switch (pageState) {
+                    case 'home':
+                        store.loadAll()
+                        history.pushState({name: 'home'}, 'Home')
+                        break
+                    case 'category':
+                        store.loadCategory(`category${params}`)
+                        history.pushState({name: 'category', category: `category${params}`}, `Category${params}`)
+                        break
+                    case 'cart':
+                        cart.load()
+                        history.pushState({name: 'cart'}, 'Cart')
+                        break
+                    case 'search':
+                        store.loadSearch(params)
+                        history.pushState({name: 'search', pattern: params}, 'Search')
+                        break
+                }
                 break
         }
     }
 }
-// waterfall control from initial state
-var control = new StateController()
-control.setPathState()
+var controller = new Controller()
+controller.controlLayer('dom', location.pathname)
