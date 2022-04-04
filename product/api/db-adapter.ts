f5462ce690d2ab40143fa7820d0a8047fa1d5be8
MichaelT233 <michaelthiele673@gmail.com>
2022-04-03 19:44:48 -0700

    semantic changes

--- product/api/db-adapter.ts
+++ product/api/db-adapter.ts
@@ -1,41 +1,39 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const pool = new Pool({
-    user: 'testu',
-    host: 'product-db',
-    database: 'testdb',
-    password: 'testpw',
+    user: "testu",
+    host: "product-db",
+    database: "testdb",
+    password: "testpw",
     port: 5432,
 });
 export type Query = {
-    field: string
-    value: string    
-};
+    field: string;
+    value: string; 
+}
 class Adaptee {
     async getAll(): Promise<any[]> {
-        const res = await pool.query("SELECT * FROM products;")
-        const result = res.rows
-        return result
-    };
+        const res = await pool.query("SELECT * FROM products;");
+        const result = res.rows;
+        return result;
+    }
     async getRows(query: Query): Promise<any[]> {
-        const res = await pool.query(`SELECT * FROM products WHERE ${query.field}='${query.value}';`)
-        const result = res.rows
-        return result
-    };
-    //getPrice
-};
+        const res = await pool.query(`SELECT * FROM products WHERE ${query.field}='${query.value}';`);
+        const result = res.rows;
+        return result;
+    }
+}
 interface Target {
-    readDB(): Promise<any[]>
-    readRows(query: Query): Promise<any[]>
-};
-export class Adapter implements Target {
-    adaptee = new Adaptee()
+    readDB(): Promise<any[]>;
+    readRows(query: Query): Promise<any[]>;
+}
+export class Adapter extends Adaptee implements Target {
     async readDB(): Promise<any[]> {
-        const result = await this.adaptee.getAll()
-        return result    
-    };
+        const result = await this.getAll();
+        return result;    
+    }
     async readRows(query: Query): Promise<any> {
-        const result = await this.adaptee.getRows(query)
-        return result
-    };
-};
+        const result = await this.getRows(query);
+        return result;
+    }
+}
--- product/api/main.ts
+++ product/api/main.ts
@@ -1,25 +1,25 @@
-import express = require('express');
-import { Adapter, Query } from './db-adapter';
+import express = require("express");
+import { Adapter, Query } from "./db-adapter";
 const app = express();
 const adapter = new Adapter();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     (async () => {
-        const result = await adapter.readDB()
-        res.type('json')
-        res.send(result)
+        const result = await adapter.readDB();
+        res.type("json");
+        res.send(result);
     })();
 });
-app.get('/search', (req, res) => {
+app.get("/search", (req, res) => {
     const query: Query = {
-        field: req.query.q as string,
+        field: req.query.f as string,
         value: req.query.v as string
     };
     (async () => {
-        const result = await adapter.readRows(query)
-        res.type('json')
-        res.send(result)
+        const result = await adapter.readRows(query);
+        res.type("json");
+        res.send(result);
     })();
 });
-app.listen(80, '0.0.0.0');
-console.log(`listening on 80`);
+app.listen(80, "0.0.0.0");
+console.log("listening on 80");
