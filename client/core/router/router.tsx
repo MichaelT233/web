f00b63dc8b166ea8b7834a2277c12256e4910398
MichaelT233 <michaelthiele673@gmail.com>
2022-05-04 09:31:52 -0700

    order client integration test pass

--- client/core/router/router.tsx
+++ client/core/router/router.tsx
@@ -6,6 +6,7 @@ import {
 import React from "react";
 import { Header, Footer } from "../ui-shell/shell"
 import * as Product from "../../product-client/ui/component";
+import * as Order from "../../order-client/ui/component";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -21,7 +22,8 @@ export function Router() {
 							<Route path ="/" element={<Product.Featured />} />
 							<Route path="/category/:category" element={<Product.Category />} />
 							<Route path="/search/:text" element={<Product.Search />} />
-							<Route path="/item/:id" element={<Product.Item />} />
+							<Route path="/item/:id" element={<Order.Item />} />
+                            <Route path="/cart/:token" element={<Order.CartListing />} />
 						</Routes>
 					</div>
 					<Footer />
