0b17980903c8add2ea393f78eedbb658586b611a
MichaelT233 <michaelthiele673@gmail.com>
2022-05-09 11:18:49 -0700

    added basic about+contact components and nav links

--- client/core/ui-shell/shell.tsx
+++ client/core/ui-shell/shell.tsx
@@ -16,27 +16,19 @@ export function Header() {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <div className="nav-link active" aria-current="page">Home</div>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/cart/test" className="nav-link active">Cart</Link>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link active">About</div>
-                        </li>
+                        <li className="nav-item"><Link to="/about" className="nav-link active">About</Link></li>
+                        <li className="nav-item"><Link to="/contact" className="nav-link active">Contact</Link></li>
+                        <li className="nav-item"><Link to="/cart/test" className="nav-link active">Cart</Link></li>
                         <li className="nav-item dropdown">
-                            <div className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Categories
-                            </div>
+                            <div className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</div>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <Link to="/"><li><div className="dropdown-item">Featured</div></li></Link>
+                                <li><div className="dropdown-item"><Link to="/" className="nav-link active">Featured</Link></div></li>
                                 <li><hr className="dropdown-divider"/></li>
-                                <Link to="/category/Home Decor"><li><div className="dropdown-item">Home Decor</div></li></Link>
+                                <li><div className="dropdown-item"><Link to="/category/Home Decor" className="nav-link active">Home Decor</Link></div></li>
                                 <li><hr className="dropdown-divider"/></li>
-                                <Link to="/category/Beauty"><li><div className="dropdown-item">Beauty</div></li></Link>
+                                <li><div className="dropdown-item"><Link to="/category/Beauty" className="nav-link active">Beauty</Link></div></li>
                                 <li><hr className="dropdown-divider"/></li>
-                                <Link to="/category/Toys"><li><div className="dropdown-item">Toys</div></li></Link>
+                                <li><div className="dropdown-item"><Link to="/category/Toys" className="nav-link active">Toys</Link></div></li>
                             </ul>
                         </li>
                     </ul>
@@ -54,12 +46,21 @@ export function Footer() {
         <div className="container">
             <footer className="py-3 my-4">
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Contact</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+                    <li className="nav-item"><Link to="/about" className="nav-link px-2 text-muted">About</Link></li>
+                    <li className="nav-item"><Link to="/contact" className="nav-link px-2 text-muted">Contact</Link></li>
                 </ul>
                 <p className="text-center text-muted">Michael Thiele</p>
             </footer>
         </div>
+    );
+}
+export function About() {
+    return (
+        <div>About</div>
+    );
+}
+export function Contact() {
+    return (
+        <div>Contact</div>
     );
 }
