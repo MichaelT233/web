3c246d7a63139fee9bfbd5b7b3c73693cc06792d
MichaelT233 <michaelthiele673@gmail.com>
2022-05-15 19:15:25 -0700

    update webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path")
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     devtool: false,
     entry: "./init.js",
     output: {
-        path: path.resolve(__dirname, "../server/core/gateway/public/js"),
+        path: path.resolve(__dirname, "../server/gateway/public/js"),
         filename: "client.js",
     },
 }
